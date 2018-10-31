c093b4b67744ac4f1be9b9d7c41512961975a71e
www.harryliu17.com <dreamharry17@sina.com>
2018-10-31 16:08:10 +0800

    Update JettyConfiguration.java
    update jettyConfiguration

--- springboot-jetty/src/main/java/com/liuhao/study/config/JettyConfiguration.java
+++ springboot-jetty/src/main/java/com/liuhao/study/config/JettyConfiguration.java
@@ -1,19 +1,19 @@
-package com.liuhao.study.config;
-
-import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * @Author: liuhao
- * @Date: 2018/10/31 13:58
- * @Description:
- **/
-@Configuration
-public class JettyConfiguration {
-
-    @Bean
-    public JettyEmbeddedServletContainerFactory jettyEmbeddedServletContainerFactory() {
-        return new JettyEmbeddedServletContainerFactory();
-    }
-}
+//package com.liuhao.study.config;
+//
+//import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * @Author: liuhao
+// * @Date: 2018/10/31 13:58
+// * @Description: 指定jetty版本为支持jdk1.7,不需要手动注入Jetty容器
+// **/
+//@Configuration
+//public class JettyConfiguration {
+//
+//    @Bean
+//    public JettyEmbeddedServletContainerFactory jettyEmbeddedServletContainerFactory() {
+//        return new JettyEmbeddedServletContainerFactory();
+//    }
+//}
